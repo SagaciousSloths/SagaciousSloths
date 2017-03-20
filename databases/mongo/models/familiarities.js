bd9545340ea95799d5e579b9af378fa6d2100035
jefQuery <jeffrey.milberger@gmail.com>
2017-03-20 12:31:03 -0700

    (feat) add mongo algorithm methods

--- databases/mongo/models/familiarities.js
+++ databases/mongo/models/familiarities.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 var db = require('../config');
 
 var FamiliaritySchema = mongoose.Schema({
-  StaffID: {type: String, default: '1'},
+  StaffID: {type: String, default: '0'},
     // Hard coded StaffID to 1 for now.
   StudentID: {type: String}, //change to datetime for Spreadsheet
-    // Add some random dummy data in Algorithm values
-  AlgorithmParameters: {type: Array, default: [.618, -1, .009, 3.14159]}
+  AlgoParams: {
+    bucket: {type: String, default: 'red'}
+  }
 });
 
 var Familiarities = mongoose.model('Familiarities', FamiliaritySchema);
@@ -14,23 +15,46 @@ var Familiarities = mongoose.model('Familiarities', FamiliaritySchema);
 module.exports = {
 
   addFamiliarity: function (userId, cardId, algoData) {
-    // For use when a card familiarity is not found for a user
+    module.exports.populateDB([{
+      StaffID: userId,
+      StudentID: cardId,
+      AlgoParams: algoData
+    }], ((result) => console.log(result))
+    );
   },
 
   addFamiliarities: function(familiarities) {  
-    // Called when a user registers on site
-    // familiarities: [{userId: val,cardId: val, algoData: {obj}}, ...]
-
+    module.exports.populateDB(familiarities, 
+      ((result) => console.log(result))
+    );
   },
 
-  
-
-  getCardAlgoData: function (userId) {},
+  getCardAlgoData: function (userId, cb) {
+    module.exports.findCard({StaffID: userId}, cb);
+  },
 
   // query Familiarities from highest to lowest score for user
   // where algoData.bucket is not green
   // into ordered array of cardIds, highest red score first
-  getOrderedCardIds(userId) {},
+  getOrderedCardIds(userId, cb) {
+  //query Familiarities for scores on all cards for the given user:
+  //-> cardScores = {cardId: score, ...}, for the given user  
+    module.exports.findCard(
+      {StaffID: userId},
+      function(result) {
+        //result = result.sort((a, b) => b.AlgoParams.bucket - a.AlgoParams.bucket);
+        var cardScores = [];
+        result.forEach(function(cardInfo) {
+          var cardScore = {};
+          cardScore[cardInfo.StudentID] = cardInfo.AlgoParams.bucket;
+          if (cardInfo.AlgoParams.bucket !== 'green') {
+            cardScores.push(cardScore);
+          }
+        });
+        cb(cardScores);
+      }); 
+      
+  },
 
   populateDB: function(newCards, cb) {
     let mongoDocCards = [];
@@ -56,7 +80,6 @@ module.exports = {
       if (error) {
         console.error(error);
       } else {
-        console.log(result);
         cb(result);
       }
     });
@@ -67,7 +90,6 @@ module.exports = {
       if (error) {
         console.error(error);
       } else {
-        console.log(result);
         cb(result);
       }
     });
@@ -75,22 +97,22 @@ module.exports = {
   //Build query to update cards ALGO data based on the StudentID and StaffID
   //insert document for card for student and teacher &or update to 
   findCard: function(query, cb) { //structure: {StudentID: 3-15-2017 12:30:01, StaffID:1}
+    
     Familiarities.find(query, function(error, result) { 
-
       if (error) {
         console.error(error);
       } else {
-        console.log(result);
         cb(result);
       }
     });
+
   }
 };
 
 
-// let cb = ((result) => result);
-// let newCards = [{StudentID: 'Jeff'}, {StudentID: 'David'}, {StudentID: 'JG'}, {StudentID: 'Kay'}];
+// let cb = ((result) => console.log(result));
+// let newCards = [{StudentID: 'Jeff', AlgoParams: {bucket: 'green'}}, {StudentID: 'David'}, {StudentID: 'JG'}, {StudentID: 'Kay'}];
 // module.exports.dropDB(cb);
 // module.exports.populateDB(newCards, cb);
 // module.exports.findAll(cb);
-// module.exports.findCard({StudentID: 'Jeff'}, cb);
+// module.exports.getOrderedCardIds(0, cb);
