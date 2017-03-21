ea9d9bc0a16d380811fa0b2d3bb1926ff24c24e7
jefQuery <jeffrey.milberger@gmail.com>
2017-03-21 12:31:25 -0700

    add mongo env var for deployment

--- databases/mongo/config.js
+++ databases/mongo/config.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // Form Mongo database with mongoose in index.js
-mongoose.connect('mongodb://localhost/memo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/memo');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
