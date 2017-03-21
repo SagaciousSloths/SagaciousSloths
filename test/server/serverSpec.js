fc51783ed55208f4e502a2d28558c11d488bc113
jefQuery <jeffrey.milberger@gmail.com>
2017-03-20 21:34:04 -0700

    (feat) changed port to process var for deployment

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -1,23 +1,24 @@
 var request = require('request');
 var expect = require('chai').expect;
+var port = process.env.port || 3000;
 
 describe('dashboard GET', function() {
   it('should respond to GET requests for /dashboard with a 200 status code', function(done) {
-    request('http://127.0.0.1:3000/dashboard', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/dashboard', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should send back parsable stringified JSON', function(done) {
-    request('http://127.0.0.1:3000/dashboard', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/dashboard', function(error, response, body) {
       expect(JSON.parse.bind(this, body)).to.not.throw();
       done();
     });
   });
 
   it('should send back an object', function(done) {
-    request('http://127.0.0.1:3000/dashboard', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/dashboard', function(error, response, body) {
       var parsedBody = JSON.parse(body);
       expect(parsedBody).to.be.an('object');
       done();
@@ -25,7 +26,7 @@ describe('dashboard GET', function() {
   });
 
   it('should send an object containing objects', function(done) {
-    request('http://127.0.0.1:3000/dashboard', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/dashboard', function(error, response, body) {
       var parsedBody = JSON.parse(body);
       expect(parsedBody).to.be.an('object');
       var keys = Object.keys(parsedBody);
@@ -37,21 +38,21 @@ describe('dashboard GET', function() {
 
 describe('quiz GET', function() {
   it('should respond to GET requests for /quiz with a 200 status code', function(done) {
-    request('http://127.0.0.1:3000/quiz', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/quiz', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should send back parsable stringified JSON', function(done) {
-    request('http://127.0.0.1:3000/quiz', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/quiz', function(error, response, body) {
       expect(JSON.parse.bind(this, body)).to.not.throw();
       done();
     });
   });
 
   it('should send back an array', function(done) {
-    request('http://127.0.0.1:3000/quiz', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/quiz', function(error, response, body) {
       var parsedBody = JSON.parse(body);
       expect(parsedBody).to.be.an('array');
       done();
@@ -59,7 +60,7 @@ describe('quiz GET', function() {
   });
 
   it('should send an array containing objects', function(done) {
-    request('http://127.0.0.1:3000/quiz', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/quiz', function(error, response, body) {
       var parsedBody = JSON.parse(body);
       expect(parsedBody).to.be.an('array');
       expect(parsedBody[0]).to.be.an('object');
@@ -69,7 +70,7 @@ describe('quiz GET', function() {
 
 
   it('Should 404 when asked for a nonexistent endpoint', function(done) {
-    request('http://127.0.0.1:3000/iPreferAnki', function(error, response, body) {
+    request('http://127.0.0.1:' + port + '/iPreferAnki', function(error, response, body) {
       expect(response.statusCode).to.equal(404);
       done();
     });
@@ -79,7 +80,7 @@ describe('quiz GET', function() {
 
   // it('should accept POST requests to /quiz', function(done) {
   //   var requestParams = {method: 'POST',
-  //     uri: 'http://127.0.0.1:3000/quiz',
+  //     uri: 'http://127.0.0.1:' + port + '/quiz',
   //     json: [{
   //       firstname: 'J-G',
   //       lastname: 'Demathieu',
@@ -94,7 +95,7 @@ describe('quiz GET', function() {
 
   // it('should respond with messages that were previously posted', function(done) {
   //   var requestParams = {method: 'POST',
-  //     uri: 'http://127.0.0.1:3000/quiz',
+  //     uri: 'http://127.0.0.1:' + port + '/quiz',
   //     json: [{
   //       firstname: 'J-G',
   //       lastname: 'Demathieu',
@@ -102,7 +103,7 @@ describe('quiz GET', function() {
   //   };
 
   //   request(requestParams, function(error, response, body) {
-  //     request('http://127.0.0.1:3000/quiz', function(error, response, body) {
+  //     request('http://127.0.0.1:' + port + '/quiz', function(error, response, body) {
   //       var parsedBody = JSON.parse(body);
   //       expect(parsedBody[0].firstname).to.equal('J-G');
   //       expect(parsedBody[0].lastname).to.equal('Demathieu');
